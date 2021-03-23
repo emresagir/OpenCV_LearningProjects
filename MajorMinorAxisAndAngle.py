41e5a9eeb6645d4ad8d57137c97dbe7ea1df063b
emresagir <emresagir1310@gmail.com>
2021-03-23 17:07:37 +0300

    Small changes

--- MajorMinorAxisAndAngle.py
+++ MajorMinorAxisAndAngle.py
@@ -1,7 +1,9 @@
 from cv2 import cv2 as cv
 import numpy as np 
+import time
 
-capture = cv.VideoCapture(0)
+def empty(a):
+    pass
 
 def getContours(imgCanny, imgOrg):
     contours, hierarchy = cv.findContours(imgCanny, cv.RETR_TREE, cv.CHAIN_APPROX_NONE) #SIMPLE olunca düz çizgilerin
@@ -15,19 +17,39 @@ def getContours(imgCanny, imgOrg):
             print("cnt area = ", area)
             cv.drawContours(imgOrg, cnt, -1, (255,0,0), 3)
             (x,y),(MA,ma),angle = cv.fitEllipse(cnt)
-            print("angle", angle)
+            ellipse = cv.fitEllipse(cnt)
+            cv.ellipse(imgOrg, ellipse, (0,255,0), 2)
+            print("angle", angle)   
+            print("MA", MA)
+            print("ma", ma)
+            #time.sleep(0.2)
+            return angle
     
+capture = cv.VideoCapture(0)
+cv.namedWindow("TrackBars")
+cv.resizeWindow("TrackBars",640,240)
+cv.createTrackbar("First Min","TrackBars",100,333,empty) 
+cv.createTrackbar("Sec Min","TrackBars",200,333,empty)
+
+font = cv.FONT_HERSHEY_SIMPLEX
 
 
 while True:
     isTrue, frame = capture.read()
     
+    
+    f_min = cv.getTrackbarPos("First Min", "TrackBars") #adjustable treshold values.
+    s_min = cv.getTrackbarPos("Sec Min", "TrackBars")
+    
+    
     imgOrg = frame
     imgGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    imgCanny = cv.Canny(imgGray, 90,90)
+    imgCanny = cv.Canny(imgGray, f_min, s_min)
 
-    getContours(imgCanny, imgOrg)
-
+    angle = getContours(imgCanny, imgOrg)
+    strAngle = str(angle)
+    cv.putText(imgOrg, strAngle, (50,50), font, 1, (0,255,255), 2)
+    
 
     cv.imshow("Video", imgCanny)
     cv.imshow("Original Video", imgOrg)
